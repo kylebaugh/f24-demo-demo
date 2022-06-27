3a65b9cfa32b7c3af75fedce51fd2ae19bf10c33
kylebaugh <kylebaugh1@gmail.com>
2022-06-27 16:21:50 -0600

    asdfasdf

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const path = require('path')
+
+// app.use(express.json())
+// app.use(cors())
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/index/html'))
+// })
+
+// const port = process.env.PORT 
+
+// port ? SERVER_PORT = port : SERVER_PORT = 4444
+
+// app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
+
 const express = require('express')
-const app = express()
-const cors = require('cors')
 const path = require('path')
 
-app.use(express.json())
-app.use(cors())
+const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index/html'))
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
-
-const port = process.env.PORT 
-
-port ? SERVER_PORT = port : SERVER_PORT = 4444
-
-app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
