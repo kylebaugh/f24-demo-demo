4bb5d0e463f377ac7dc4d307f944c6d366c3e2f5
kylebaugh <kylebaugh1@gmail.com>
2022-06-27 16:19:51 -0600

    adsf

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index/html'))
 })
 
 const {port} = process.env.PORT 
